019ac4fe0a8005d16bc86fc753bf60e8680cc0d2
Pavel <impetus@yandex.ru>
2020-10-05 15:23:19 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] x = { {4,5,6}, {9,8,2}, {4,2,88}};
+        printMatr(x);
+        // элементы главной диагонали i=j
+        //mainDiagMatr(x);
+        // элементы побочной диагонали i+j = n - 1 (где n - размер кв. матрицы)
+        // pobochDiagMatr(x);
+        // элементы выше главное диагонали i<j
+      //  upDiagMatr(x);
+       // System.out.println();
+        // элементы ниже главное диагонали i>j
+       // downDiagMatr(x);
+        // элементы выше побочной диагонали i+j < n - 1
+        //upPobochDiagMatr(x);
+       // System.out.println();
+        // элементы ниже побочной диагонали i+j > n - 1
+        //downPobochDiagMatr(x);
+    }
+
+    public static void upPobochDiagMatr(int[][] x) {
+        for(int i=0;i<x.length;i++) {
+            for(int j=0;j<x[i].length;j++) {
+                if(i + j < x[i].length - 1) System.out.print(x[i][j] + " ");
+            }
+        }
+    }
+
+    public static void downPobochDiagMatr(int[][] x) {
+        for(int i=0;i<x.length;i++) {
+            for(int j=0;j<x[i].length;j++) {
+                if(i + j > x[i].length - 1) System.out.print(x[i][j] + " ");
+            }
+        }
+    }
+
+    public static void downDiagMatr(int[][] x) {
+        for(int i=0;i<x.length;i++) {
+            for(int j=0;j<x[i].length;j++) {
+                if(i > j) System.out.print(x[i][j] + " ");
+            }
+        }
+    }
+
+    public static void upDiagMatr(int[][] x) {
+        for(int i=0;i<x.length;i++) {
+            for(int j=0;j<x[i].length;j++) {
+                if(i < j) System.out.print(x[i][j] + " ");
+            }
+        }
+    }
+
+    public static void pobochDiagMatr(int[][] x) {
+        for(int i=0;i<x.length;i++) {
+            for(int j=0;j<x[i].length;j++) {
+                if(i+j == x[i].length-1) System.out.print(x[i][j] + " ");
+            }
+        }
+    }
+
+    public static void mainDiagMatr(int[][] x) {
+        for(int i=0;i<x.length;i++) {
+            for(int j=0;j<x[i].length;j++) {
+                if(i==j) System.out.print(x[i][j] + " ");
+            }
+
+        }
+    }
+
+    public static void printMatr(int[][] x) {
+        for(int i=0;i<x.length;i++) {
+            for(int j=0;j<x[i].length;j++) {
+                System.out.print(x[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
